7e3e1bc1f218e6117f1d101ae4c38de5d53248dd
Ana Julia <anajuliadefendi@gmail.com>
2024-06-22 19:46:27 -0300

    arquivos
    Criação dos arquivos css e js

---
+++ scripts.js
@@ -0,0 +1,4 @@
+const form = document.getElementById("form");
+const mascara = form.elements['masc'].value;
+const subrede = form.elements['subrede'].value;
+const endereco = form.elements['endereco'].value;
