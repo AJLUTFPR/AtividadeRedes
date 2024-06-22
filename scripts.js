e3637877feae9944371a99db2a59517113aa3ffa
Ana Julia <anajuliadefendi@gmail.com>
2024-06-22 20:10:41 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,14 +4,21 @@ let subrede = form.elements['subrede'];
 let endereco = form.elements['endereco'];
 let btnLimpar = document.getElementById("limpar")
 
+btnLimpar.addEventListener("click", handleBtnLimpar)
+
+
 function handleBtnLimpar(){
     if(subrede.value !== '' || endereco.value !== ''){
         subrede.value = ''
         endereco.value = ''
     }
+
+    if(mascara.value !== '1'){
+        mascara.value = "1"
+    }
+
     handleBtnLimpar()
 }
 
 
-btnLimpar.addEventListener("click", handleBtnLimpar)
 
